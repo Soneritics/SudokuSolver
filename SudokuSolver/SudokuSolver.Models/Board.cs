8127d5298846d0a7ae365ac50f658f9d6d762ebe
Jordi Jolink <info@soneritics.nl>
2019-02-14 08:14:34 +0100

    Add board objects

---
+++ SudokuSolver/SudokuSolver.Models/Board.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuSolver.Models
+{
+    public class Board
+    {
+        private BoardValue[,] _board;
+
+        public int Lines { get; private set; } = 3 * 3;
+        public int Columns { get; private set; } = 3 * 3;
+
+        // Create an empty board
+        public Board()
+        {
+            Clear();
+        }
+
+        // Return the BoardValue of a specific line and column
+        public BoardValue GetBoardValue(int line, int column)
+        {
+            return _board[line, column];
+        }
+
+        // Set board value for a specific column on a specific line
+        public void SetBoardValue(int line, int column, BoardValue value)
+        {
+            _board[line, column] = value;
+        }
+
+        // Clear the board
+        public void Clear()
+        {
+            _board = new BoardValue[Lines, Columns];
+
+            for (var line = 0; line < Lines; line++)
+            {
+                for (var column = 0; column < Columns; column++)
+                    _board[line, column] = new BoardValue();
+            }
+        }
+    }
+}
---
+++ SudokuSolver/SudokuSolver.Models/BoardValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuSolver.Models
+{
+    public class BoardValue
+    {
+        public int Value { get; set; }
+        public bool IsProvided { get; set; }
+
+        public BoardValue()
+        {
+            IsProvided = false;
+            Value = 0;
+        }
+
+        public BoardValue(int value)
+        {
+            IsProvided = true;
+            Value = value;
+        }
+    }
+}
