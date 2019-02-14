71af14c847f5d86844c6e798e190055be1659165
Jordi Jolink <info@soneritics.nl>
2019-02-14 20:32:51 +0100

    Add Solver interface

---
+++ SudokuSolver/SudokuSolver.Business/Interfaces/ISolver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SudokuSolver.Models;
+
+namespace SudokuSolver.Business.Interfaces
+{
+    public interface ISolver
+    {
+        Board Solve(Board board);
+    }
+}
